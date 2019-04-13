854653357c8e744cc1829b59a7fd8e61ab0841e7
Apoorv <vapoorve@gmail.com>
2019-04-13 19:28:09 +0530

    Render Template

--- [2] Render Template/app.py
+++ [2] Render Template/app.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def Home():
     return render_template('index.html')
+@app.route('/about')
+def About():
+    return render_template('about.html')
 
 app.run(debug=True)
