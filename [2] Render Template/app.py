f1106f9867368936a9e910c92933255f95834fda
Apoorv <vapoorve@gmail.com>
2019-04-13 19:53:01 +0530

    Render Template

--- [2] Render Template/app.py
+++ [2] Render Template/app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def Home():
     PageData = "This is Test webapp "
-    return render_template('index.html',Data = PageData)
+    return render_template('pages/index.html',Data = PageData)
 @app.route('/about')
 def About():
     PageData = "Hi my name is Apoorv Verma"
-    return render_template('about.html',Data = PageData)
+    return render_template('pages/about.html',Data = PageData)
 
 app.run(debug=True)
