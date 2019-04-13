8553b5df4cb02644eb26d8c37bd92c4feaebbef0
Apoorv <vapoorve@gmail.com>
2019-04-13 21:16:52 +0530

    Jinja Templating

--- [3] Jinja Template Engine/app.py
+++ [3] Jinja Template Engine/app.py
@@ -3,6 +3,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def Home():
-    return  render_template('pages/index.html')
+    Homeactive = 'active'
+    return  render_template('pages/index.html',Homeactive =Homeactive)
+
+@app.route('/about')
+def About():
+    Aboutactive = 'active'
+    return render_template('pages/about.html',Aboutactive = Aboutactive)
+
+@app.route('/login')
+def Login():
+    Loginactive = 'active'
+    return render_template('pages/login.html',Loginactive = Loginactive)
+
+@app.route('/signup')
+def Signup():
+    Signupactive = 'active'
+    return render_template('pages/signup.html',Signupactive = Signupactive)
 
 app.run(debug=True)
